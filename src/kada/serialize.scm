1e5ad16e9d0e183846dd2bc6ab51b0651f42eca4
atmatm9182 <atmatm9182@gmail.com>
2024-12-06 19:48:44 +0100

    Fix bugs with serialization of spans into csv.

--- src/kada/serialize.scm
+++ src/kada/serialize.scm
@@ -1,18 +1,22 @@
 (define-module (kada serialize)
+  #:use-module ((srfi srfi-1))
   #:use-module (ice-9 match)
   #:use-module (kada types)
   #:use-module (kada db)
 
-  #:export (serialize-to-csv-buf))
+  #:export (serialize-to-csv))
 
-(define (serialize-to-csv-buf spans)
+(define (serialize-to-csv spans)
   (string-append csv-header-row
-                 (fold string-append "" span->csv-row)))
+                 (fold (lambda (span acc)
+                         (string-append acc (span->csv-row span)))
+                       ""
+                       spans)))
 
 (define (span->csv-row span)
-  (format #f "%s;%s;%s\n"
-          (ts->string (span-start span))
-          (ts->string (span-end span))
+  (format #f "~a;~a;~a\n"
+          (ts->string (span-started span))
+          (ts->string (span-ended span))
           (span-name span)))
 
 (define csv-header-row
